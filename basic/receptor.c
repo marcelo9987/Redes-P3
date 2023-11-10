5cfba478d0fa0dd7364bee285c2ad0dc8e93e457
Pedro Vidal Villalba <pedrovidalvillalba@gmail.com>
2023-11-10 14:03:46 +0100

    Quitado while del receptor

--- basic/receptor.c
+++ basic/receptor.c
@@ -7,13 +7,15 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-//todo: valor temporal para que compile. Hay que buscarle sitio y valor adecuado
+//TODO: valor temporal para que compile. Hay que buscarle sitio y valor adecuado
 #define MAX_BYTES_RECVFROM 128
 
 #include "host.h"
 #include "loging.h"
 
 #define MAX_BYTES_RECV 128
+#define DEFAULT_OWN_PORT 8080
+#define DEFAULT_LOG_FILE "receptor.log"
 
 /**
  * Estructura de datos para pasar a la función process_args.
@@ -23,10 +25,11 @@
  */
 struct arguments {
     int argc;
-    char **argv;
-    uint16_t *self_port;
-    char **log_file;
+    char** argv;
+    uint16_t* own_port;
+    char** log_file;
 };
+
 
 /**
  * @brief   Procesa los argumentos del main.
@@ -37,8 +40,8 @@ struct arguments {
  * @param args  Estructura con los argumentos del programa y punteros a las
  *              variables que necesitan inicialización.
  */
-
 static void process_args(struct arguments args);
+
 
 /**
  * @brief Imprime la ayuda del programa.
@@ -47,81 +50,78 @@ static void process_args(struct arguments args);
  */
 static void print_help(char *exe_name);
 
-/**
- * @brief   Procesa los argumentos del main.
- *
- * Procesa los argumentos proporcionados al programa por línea de comandos,
- * e inicializa las variables del programa necesarias acorde a estos.
- *
- * @param args  Estructura con los argumentos del programa y punteros a las
- *              variables que necesitan inicialización.
- */
+
+/***** TODO: comentar **/
 void handle_data(Host self, Host *other);
 
-int main(int argc, char *argv[])
-{
+
+
+int main(int argc, char** argv) {
     Host receiver;
-    uint16_t self_port;
+    uint16_t own_port;
     char *log_file;
     struct arguments args = {
-            .argc = argc,
-            .argv = argv,
-            .self_port = &self_port,
-            .log_file = &log_file
+        .argc = argc,
+        .argv = argv,
+        .own_port = &own_port,
+        .log_file = &log_file
     };
 
     set_colors();
 
     process_args(args);
 
-    receiver = create_own_host(AF_INET, SOCK_DGRAM, 0, self_port, log_file);
+    receiver = create_own_host(AF_INET, SOCK_DGRAM, 0, own_port, log_file);
 
-    Host other;
-
-    handle_data(receiver, &other);
+    handle_data(receiver);
 
     close_host(&receiver);
-    close_host(&other);
 
     exit(EXIT_SUCCESS);
 }
 
-void handle_data(Host self, Host* other) {
+
+void handle_data(Host self) {
     ssize_t received_bytes = 0;
     char received_messsage[MAX_BYTES_RECVFROM];
+    struct sockaddr_in other_address;
+    socklen_t size_addr = sizeof(struct sockaddr_in);
 
-    int size_addr = sizeof(struct sockaddr);
-    while ( (received_bytes = recvfrom(self.socket, received_messsage, MAX_BYTES_RECV, 0, (struct sockaddr *) &(other->address), (socklen_t *) &size_addr)) != 0 ) {
-        // Se recibe hasta finalizar conexión
-        if (received_bytes < 0) {
-            fail("Error en la recepción del mensaje");
-        }
-
-        printf("Mensaje recibido : %s\n", received_messsage);
-        printf("Han sido recibidos %ld bytes.\n", received_bytes);
-
+    if( (received_bytes = recvfrom(self.socket, received_messsage, MAX_BYTES_RECV, 0, (struct sockaddr *) &(other_address), &size_addr)) < 0 ) {
+        log_printf_err(self.log, "Error en la recepción del mensaje\n");
+        fail("Error en la recepción del mensaje");
     }
-    *other = create_remote_host(AF_INET, SOCK_DGRAM, 0, inet_ntoa(other->address.sin_addr), ntohs(other->address.sin_port));
+
+    printf("Mensaje recibido : %s\n", received_messsage);
+    printf("Han sido recibidos %ld bytes.\n", received_bytes);
+    log_printf(self.log, "Mensaje recibido : %s\tHan sido recibidos %ld bytes.\n", received_messsage, received_bytes);
 }
+
 
 static void print_help(char *exe_name) {
     /** Cabecera y modo de ejecución **/
-    printf("Uso: %s [-p] <port> [-h]", exe_name);
+    printf("Uso: %s [-p] <port> [-l <log> | --no-log] [-h]", exe_name);
 
     /** Lista de opciones de uso **/
     printf(" Opción\t\tOpción larga\t\tSignificado\n");
-    printf(" -p <port>\t--port <port>\t\tPuerto en el que escucha el servidor al que conectarse.\n");
+    printf(" -p <port>\t--port <port>\t\tPuerto en el que escucha el receptor al que conectarse.\n");
+    printf(" -l <log>\t--log <log>\t\tNombre del archivo en el que guardar el registro de actividad del receptor.\n");
+    printf(" -n\t\t--no-log\t\tNo crear archivo de registro de actividad.\n");
     printf(" -h\t\t--help\t\t\tMostrar este texto de ayuda y salir.\n");
 
     /** Consideraciones adicionales **/
-    printf("\nPueden especificarse los parámetros <port> para el puerto en los que escucha el servidor sin escribir la opción '-p', siempre y cuando esta sea el primer parámetro que se pasa a la función.\n");
-    printf("\nSi se especifica varias veces un argumento, el comportamiento está indefinido.\n");
+    printf("\nPueden especificarse los parámetros <port> para el puerto en los que escucha el receptor sin escribir la opción '-p', siempre y cuando esta sea el primer parámetro que se pasa a la función.\n");
+    printf("\nSi se especifica varias veces un argumento, o se especifican las opciones \"--log\" y \"--no-log\" a la vez, el comportamiento está indefinido.\n");
 }
+
 
 static void process_args(struct arguments args) {
     char *current_arg;
-    uint8_t set_port = 0;   /* Flags para saber si se setearon la IP y puerto */
     int i;
+
+    /* Inicializar los valores de puerto y backlog a sus valores por defecto */
+    *args.own_port = DEFAULT_OWN_PORT;
+    *args.log_file = DEFAULT_LOG_FILE;
 
     for (i = 1; i < args.argc; i++) {
         /* Procesamos los argumentos (sin contar el nombre del ejecutable) */
@@ -137,21 +137,30 @@ static void process_args(struct arguments args) {
 
             switch (current_arg[1]) {
                 case 'p':   /* Puerto */
-                    if (++i < args.argc)
-                    {
-                        *args.self_port = atoi(args.argv[i]);
-                        if (*args.self_port < 0) {
+                    if (++i < args.argc) {
+                        *args.own_port = atoi(args.argv[i]);
+                        if (*args.own_port < 0) {
                             fprintf(stderr, "El valor de puerto especificado (%s) no es válido.\n\n", args.argv[i]);
                             print_help(args.argv[0]);
                             exit(EXIT_FAILURE);
                         }
-                        set_port = 1;
-                    } else
-                    {
+                    } else {
                         fprintf(stderr, "Puerto no especificado tras la opción '-p'.\n\n");
                         print_help(args.argv[0]);
                         exit(EXIT_FAILURE);
                     }
+                    break;
+                case 'l':   /* Log */
+                    if (++i < args.argc) {
+                        *args.log_file = args.argv[i];
+                    } else {
+                        fprintf(stderr, "Nombre del log no especificado tras la opción '-l'.\n\n");
+                        print_help(args.argv[0]);
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                case 'n':   /* No-log */
+                    *args.log_file = NULL;
                     break;
                 case 'h':   /* Ayuda */
                     print_help(args.argv[0]);
@@ -162,19 +171,12 @@ static void process_args(struct arguments args) {
                     exit(EXIT_FAILURE);
             }
         } else if (i == 1) {    /* Se especificó el puerto como primer argumento */
-            *args.self_port = atoi(args.argv[i]);
-            if (*args.self_port < 0) {
+            *args.own_port = atoi(args.argv[i]);
+            if (*args.own_port < 0) {
                 fprintf(stderr, "El valor de puerto especificado (%s) no es válido.\n\n", args.argv[i]);
                 print_help(args.argv[0]);
                 exit(EXIT_FAILURE);
             }
-            set_port = 1;
         }
-    }
-
-    if (!set_port) {
-        fprintf(stderr, "%s\n", "No se especificó el puerto del servidor al que conectarse.\n");
-        print_help(args.argv[0]);
-        exit(EXIT_FAILURE);
     }
 }
