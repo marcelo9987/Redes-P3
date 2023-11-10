54b8540fe94c5785389f79597dc48e31c052a978
Pedro Vidal Villalba <pedrovidalvillalba@gmail.com>
2023-11-10 11:20:59 +0100

    Cabecera de host definida

--- host/host.h
+++ host/host.h
@@ -23,9 +23,43 @@ typedef struct {
 } Host;
 
 
-Host create_self_host();
+/**
+ * @brief   Crea un host del propio programa.
+ *
+ * Crea un host nuevo con un nuevo socket, y le asigna un puerto.
+ * Si el argumento logfile no es NULL, crea también un archivo de log para guardar un registro de actividad.
+ *
+ * @param domain    Dominio de comunicación. 
+ * @param type      Tipo de protocolo usado para el socket.
+ * @param protocol  Protocolo particular a usar en el socket. Normalmente solo existe
+ *                  un protocolo para la combinación dominio-tipo dada, en cuyo caso se
+ *                  puede especificar con un 0.
+ * @param port      Número de puerto en el que escuchar (en orden de host).
+ * @param logfile   Nombre del archivo en el que guardar el registro de actividad.
+ *
+ * @return  Host que guarda toda la información relevante sobre sí mismo con la que
+ *          fue creado, y con un socket abierto y conectado por el puerto  especificado.
+ */
+Host create_self_host(int domain, int type, int protocol, uint16_t port, char* logfile);
 
-Host create_peer_host();
+
+/**
+ * @brief   Crea un host remoto.
+ *
+ * Crea un host nuevo con la información del host remoto al que se envían
+ * o del que se reciben datos.
+ *
+ * @param domain    Dominio de comunicación. 
+ * @param type      Tipo de protocolo usado para el socket.
+ * @param protocol  Protocolo particular a usar en el socket. Normalmente solo existe
+ *                  un protocolo para la combinación dominio-tipo dada, en cuyo caso se
+ *                  puede especificar con un 0.
+ * @param ip        IP del host remoto (en formato textual).
+ * @param port      Número de puerto en el que escucha el host remoto (en orden de host).
+ *
+ * @return  Host con toda la información relevante y disponible sobre el host remoto.
+ */
+Host create_remote_host(int domain, int type, int protocol, char* ip, uint16_t port);
 
 
 /**
